b6e32feec96d2fbd6258758be7d7f25c34be453f
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-03-22 16:37:35 +0000

    mvp of pokemon list

--- app/src/main/java/io/github/joaogouveia89/retedex/pokemon/presentation/components/PokemonContent.kt
+++ app/src/main/java/io/github/joaogouveia89/retedex/pokemon/presentation/components/PokemonContent.kt
@@ -5,11 +5,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.paging.compose.LazyPagingItems
 import io.github.joaogouveia89.retedex.core.domain.model.Pokemon
 import io.github.joaogouveia89.retedex.ui.theme.BlueSecondary
@@ -33,7 +31,10 @@ fun PokemonContent(
                 val pokemonNullable = pagingPokemons[index]
 
                 pokemonNullable?.let { pokemon ->
-                    Text(text = pokemon.name, color = white)
+                    PokemonItem(
+                        name = pokemon.name,
+                        index = index + 1
+                    )
                 }
             }
         }
---
+++ app/src/main/java/io/github/joaogouveia89/retedex/pokemon/presentation/components/PokemonItem.kt
@@ -0,0 +1,61 @@
+package io.github.joaogouveia89.retedex.pokemon.presentation.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import io.github.joaogouveia89.retedex.ui.theme.BluePrimary
+import io.github.joaogouveia89.retedex.ui.theme.black
+
+@Composable
+fun PokemonItem(
+    modifier: Modifier = Modifier,
+    index: Int,
+    name: String,
+) {
+    Box(
+        modifier = modifier
+            .background(BluePrimary)
+    ) {
+        Card(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(6.dp),
+            shape = RoundedCornerShape(8.dp),
+            elevation = 8.dp
+        ) {
+            Row(
+                modifier = Modifier
+                    .padding(12.dp)
+            ) {
+                Text(
+                    text = "$index - $name",
+                    color = black,
+                    fontFamily = FontFamily.SansSerif,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 25.sp
+                )
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PokemonItemPreview() {
+    PokemonItem(
+        index = 4,
+        name = "Charmander"
+    )
+}
