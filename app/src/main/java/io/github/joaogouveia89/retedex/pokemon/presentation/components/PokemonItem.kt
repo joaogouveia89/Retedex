6113a086f9f0ced02da4c2f04ce949dcc7d9c19a
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-03-22 18:12:05 +0000

    added pokeball to pokemon list

--- app/src/main/java/io/github/joaogouveia89/retedex/pokemon/presentation/components/PokemonItem.kt
+++ app/src/main/java/io/github/joaogouveia89/retedex/pokemon/presentation/components/PokemonItem.kt
@@ -11,11 +11,17 @@ import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import io.github.joaogouveia89.retedex.R
+import io.github.joaogouveia89.retedex.core.presentation.common.AsyncImageUrl
 import io.github.joaogouveia89.retedex.ui.theme.BluePrimary
 import io.github.joaogouveia89.retedex.ui.theme.black
 
@@ -49,7 +55,22 @@ fun PokemonItem(
                     color = black,
                     fontFamily = FontFamily.SansSerif,
                     fontWeight = FontWeight.Medium,
-                    fontSize = 25.sp
+                    fontSize = 25.sp,
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+
+                // TODO: if the pokemon is a favorite one, show the sprites of it
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("")
+                        .crossfade(true)
+                        .error(R.drawable.pokeball)
+                        .placeholder(R.drawable.pokeball)
+                        .build(),
+                    contentDescription = "",
+                    contentScale = ContentScale.FillHeight,
+                    modifier = modifier
+                        .padding(start = 6.dp)
                 )
             }
         }
