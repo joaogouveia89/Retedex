6cb56e70c1b21b042b77c4c7b27dccbfa216201a
Joao Gouveia <j.lucas.gouveia@gmail.com>
2024-03-23 14:36:07 +0000

    AddPokemonFavoriteUseCase

---
+++ app/src/main/java/io/github/joaogouveia89/retedex/pokemonFavorite/data/usecase/AddPokemonFavoriteUseCaseImpl.kt
@@ -0,0 +1,20 @@
+package io.github.joaogouveia89.retedex.pokemonFavorite.data.usecase
+
+import io.github.joaogouveia89.retedex.core.util.ResultData
+import io.github.joaogouveia89.retedex.pokemonFavorite.domain.repository.PokemonFavoriteRepository
+import io.github.joaogouveia89.retedex.pokemonFavorite.domain.usecase.AddPokemonFavoriteUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import javax.inject.Inject
+
+class AddPokemonFavoriteUseCaseImpl @Inject constructor(
+    private val repository: PokemonFavoriteRepository
+): AddPokemonFavoriteUseCase {
+    override suspend fun invoke(params: AddPokemonFavoriteUseCase.Params): Flow<ResultData<Unit>> =
+    flow{
+        val insert: Unit = repository.insert(params.pokemon)
+        emit(ResultData.Success(insert))
+    }.flowOn(Dispatchers.IO)
+}
--- app/src/main/java/io/github/joaogouveia89/retedex/pokemonFavorite/di/PokemonFavoriteModule.kt
+++ app/src/main/java/io/github/joaogouveia89/retedex/pokemonFavorite/di/PokemonFavoriteModule.kt
@@ -7,8 +7,10 @@ import dagger.hilt.components.SingletonComponent
 import io.github.joaogouveia89.retedex.core.data.local.daos.PokemonDao
 import io.github.joaogouveia89.retedex.pokemonFavorite.data.repository.PokemonFavoriteRepositoryImpl
 import io.github.joaogouveia89.retedex.pokemonFavorite.data.source.PokemonFavoriteLocalDataSourceImpl
+import io.github.joaogouveia89.retedex.pokemonFavorite.data.usecase.AddPokemonFavoriteUseCaseImpl
 import io.github.joaogouveia89.retedex.pokemonFavorite.domain.repository.PokemonFavoriteRepository
 import io.github.joaogouveia89.retedex.pokemonFavorite.domain.source.PokemonFavoriteLocalDataSource
+import io.github.joaogouveia89.retedex.pokemonFavorite.domain.usecase.AddPokemonFavoriteUseCase
 import javax.inject.Singleton
 
 @Module
@@ -23,4 +25,9 @@ object PokemonFavoriteModule {
     @Singleton
     fun providePokemonFavoriteRepository(localDataSource: PokemonFavoriteLocalDataSource): PokemonFavoriteRepository =
         PokemonFavoriteRepositoryImpl(localDataSource = localDataSource)
+
+    @Provides
+    @Singleton
+    fun provideAddPokemonFavoriteUseCase(repository: PokemonFavoriteRepository): AddPokemonFavoriteUseCase =
+        AddPokemonFavoriteUseCaseImpl(repository = repository)
 }
